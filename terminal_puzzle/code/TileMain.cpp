5ef77697fadcc141f7bfde5b6b3ce02912a6012e
flodieder <florian.diederichs@online.de>
2020-01-13 20:22:24 +0100

    changed to fdisk disk id and cleanup

--- terminal_puzzle/code/TileMain.cpp
+++ terminal_puzzle/code/TileMain.cpp
@@ -13,17 +13,16 @@
 #include <vector>
 #include <chrono>
 #include <thread>
-//#include "ctime"
 #include "./Tile.h"
 #include "./TileStructure.h"
 
 using namespace std;
 
 void custom_print(char* s) {
-  //clear();
-  mvprintw(100, 5, s);
+  clear();
+  mvprintw(0, 0, s);
   refresh();
-  //sleep(2);
+  sleep(2);
 }
 
 inline bool exist_test (const std::string& name) {
@@ -32,19 +31,8 @@ inline bool exist_test (const std::string& name) {
 }
 
 int main(int argc, char** argv) {
-  //init window size
-  system("/usr/bin/xvkbd -text \"\\C\\S_\"");
-  system("/usr/bin/xvkbd -text \"\\C\\S_\"");
-  system("/usr/bin/xvkbd -text \"\\C\\S_\"");
-  system("/usr/bin/xvkbd -text \"\\C\\S_\"");
-  system("/usr/bin/xvkbd -text \"\\C\\S_\"");
-  system("/usr/bin/xvkbd -text \"\\C\\S_\"");
-  system("/usr/bin/xvkbd -text \"\\C\\S_\"");
-  system("/usr/bin/xvkbd -text \"\\C\\S_\"");
-  sleep(1);
-
   // init TileStructure
-  TileStructure tstruct = TileStructure("adidas_35.txt");
+  TileStructure tstruct = TileStructure("STASIS.txt");
 
   // ncurses init stuff
   initscr();
@@ -53,184 +41,89 @@ int main(int argc, char** argv) {
   curs_set(false);
   nodelay(stdscr, true);
   keypad(stdscr, true);
-  int key;
 
   // show TileStruct before shuffeled
   clear();
   tstruct.draw(5, 5);
   refresh();
-  sleep(2);
 
-  FILE *fp_a;
-  FILE *fp_b;
-  int status;
-  char old_color_a [6];
-  char new_color_a [6];
-  char color_a [6];
-  char old_color_b [6];
-  char new_color_b [6];
-  char color_b [6];
+  // puzzle activation
+  bool puzzle_active = false;
+  while (!puzzle_active){
 
-  while (true){
-    fp_a = popen("sudo blkid /dev/sda | grep -o LABEL.* | cut -d\\\" -f2", "r");
-    fgets(color_a, 6, fp_a);
-    fp_b = popen("sudo blkid /dev/sdb | grep -o LABEL.* | cut -d\\\" -f2", "r");
-    fgets(color_b, 6, fp_b);
+    FILE *fp_a = popen("sudo fdisk -l | grep \"\/dev\/sd\\|Disk identifier: 0x00\"", "r");
+    char buffer [120];
 
-    if ((strcmp(color_a, "Rottt") == 0) || (strcmp(color_a, "Blauu") == 0) || (strcmp(color_a, "Gelbb") == 0) || (strcmp(color_a, "Gruen") == 0)){
-      tstruct.shuffle();
-      clear();
-      tstruct.draw(5, 5);
-      refresh();
-      break;
-    }
-    else if ((strcmp(color_b, "Rottt") == 0) || (strcmp(color_b, "Blauu") == 0) || (strcmp(color_b, "Gelbb") == 0) || (strcmp(color_b, "Gruen") == 0)){
-      tstruct.shuffle();
-      clear();
-      tstruct.draw(5, 5);
-      refresh();
-      break;
+    while (fgets(buffer, 120, fp_a) != NULL){
+      pclose(fp_a);
+      if(strstr(buffer, "\/dev\/sd") != NULL) {
+        tstruct.shuffle();
+        clear();
+        tstruct.draw(5, 5);
+        refresh();
+        puzzle_active = true;
+        break;
+      }
     }
   }
 
- /* TODO DELETE
-  * while (fgets(color_a, 6, fp_a) != NULL) {
-    std::copy(std::begin(color_a), std::end(color_a), std::begin(new_color_a));
-    std::copy(std::begin(color_a), std::end(color_a), std::begin(old_color_a));
-    //custom_print(new_color);
-    //custom_print(old_color);
-  }*/
-
   // start loop
   while (true) {
-    //key = getch();
-    //FILE *fp_a;
-    //int status;
 
-    // if (key == 27) break;
-    // if (test_var == 1) {
-    //     tstruct.turnTilesRight(0);
-    //     clear();
-    // }
-    // if (/*key == 's'*/exist_test("/media/floppy/gruen.txt")) {
-    //     tstruct.turnTilesRight(1);
-    //     clear();
-    // }
-    // if (/*key == 'd'*/exist_test("/media/floppy/blau.txt")) {
-    //     tstruct.turnTilesRight(2);
-    //     clear();
-    // }
-    // if (/*key == 'f'*/exist_test("/media/floppy/gelb.txt")) {
-    //     tstruct.turnTilesRight(3);
-    //     clear();
-    // }
-    // if (key == 'q') {
-    //     tstruct.turnTilesAround(0);
-    //     clear();
-    // }
-    // if (key == 'w') {
-    //     tstruct.turnTilesAround(1);
-    //     clear();
-    // }
-    // if (key == 'e') {
-    //     tstruct.turnTilesAround(2);
-    //     clear();
-    // }
-    // if (key == 'r') {
-    //     tstruct.turnTilesAround(3);
-    //     clear();
-    // }
-    // tstruct.draw(5, 5);
+    auto t1 = std::chrono::high_resolution_clock::now();
 
-    int counter = 0;
-
-    while (counter < 30) {
-      
-      if (counter == 15) {
-        fp_b = popen("sudo blkid /dev/sdb | grep -o LABEL.* | cut -d\\\" -f2", "r");
-      
-        std::copy(std::begin(new_color_b), std::end(new_color_b), std::begin(old_color_b));
-      
-        if (fgets(color_b, 6, fp_b) != NULL) {
-          std::copy(std::begin(color_b), std::end(color_b), std::begin(new_color_b));
-        }
-        else {
-          new_color_b[0] = 'X';
-          old_color_b[0] = 'X';
-        }
-        
-        if (strcmp(new_color_b, "Rottt") == 0) {
-          tstruct.turnTilesAround(0);
-          clear();
-        }
-        else if (strcmp(new_color_b, "Gruen") == 0) {
-          tstruct.turnTilesAround(1);
-          clear();
-        }
-        else if (strcmp(new_color_b, "Blauu") == 0) {
-          tstruct.turnTilesAround(2);
-         clear();
-        }
-        else if (strcmp(new_color_b, "Gelbb") == 0) {
-          tstruct.turnTilesAround(3);
-          clear();
-        }
-        status = pclose(fp_b);
-        
-        //this_thread::sleep_for(chrono::milliseconds(800));
+    FILE *fp_a = popen("sudo fdisk -l | grep \"\/dev\/sd\\|Disk identifier: 0x00\"", "r");
+    char buffer [120];
+    int sda_id = -1;
+    int sdb_id = -1;
+    bool sda_found = false;
+    bool sdb_found = false;
+    while (fgets(buffer, 120, fp_a) != NULL){
+      if(strstr(buffer, "\/dev\/sda") != NULL) {
+        sda_found = true;
       }
-      
-      if (counter == 29) {
-        fp_a = popen("sudo blkid /dev/sda | grep -o LABEL.* | cut -d\\\" -f2", "r");
-
-        std::copy(std::begin(new_color_a), std::end(new_color_a), std::begin(old_color_a));
-        if (fgets(color_a, 6, fp_a) != NULL) {
-
-          std::copy(std::begin(color_a), std::end(color_a), std::begin(new_color_a));
-        }
-        else {
-          new_color_a[0] = 'X';
-          old_color_a[0] = 'X';
-        }
-        //custom_print(new_color);
-
-        //if (strcmp(new_color, old_color) != 0) {
-          //custom_print("test");
-        if (strcmp(new_color_a, "Rottt") == 0) {
-          tstruct.turnTilesRight(0);
-          clear();
-        }
-        else if (strcmp(new_color_a, "Gruen") == 0) {
-            tstruct.turnTilesRight(1);
-            clear();
-        }
-        else if (strcmp(new_color_a, "Blauu") == 0) {
-            tstruct.turnTilesRight(2);
-            clear();
-        }
-        else if (strcmp(new_color_a, "Gelbb") == 0) {
-            tstruct.turnTilesRight(3);
-            clear();
-        }
-        //}
-        status = pclose(fp_a);
-
-        this_thread::sleep_for(chrono::milliseconds(1600));
+      else if(sda_found) {
+        sda_id = buffer[26] - '0';
+        sda_found = false;
       }
-      tstruct.draw(5, 5);
-      refresh();
-      this_thread::sleep_for(chrono::microseconds(100));
-      counter++;
+      else if(strstr(buffer, "\/dev\/sdb") != NULL) {
+        sdb_found = true;
+      }
+      else if(sdb_found) {
+        sdb_id = buffer[26] - '0';
+        sdb_found = false;
+      }
     }
+
+    pclose(fp_a);
+    if(sda_id != -1) {
+      tstruct.turnTilesRight(sda_id);
+      clear();
+    }
+
+    if(sdb_id != -1) {
+      tstruct.turnTilesAround(sdb_id);
+      clear();
+    }
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+       
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    auto sleep_duration = 1000000-duration;
+    if (sleep_duration > 0) {
+      usleep(sleep_duration);
+    }
+
+    tstruct.draw(5, 5);
+    refresh();    
     
-    refresh();
     if (tstruct.solved()) {
-        sleep(1);
-        clear();
-        mvprintw(5, 5, "YOU SOLVED IT");
-        refresh();
-        sleep(5);
-        break;
+      sleep(1);
+      clear();
+      mvprintw(5, 5, "YOU SOLVED IT");
+      refresh();
+      sleep(5);
+      break;
     }
   }
   endwin();
